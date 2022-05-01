6da530f330f4f2a7703273b5dc5e01f810c1a5cd
hochyard <73438069+hochyard@users.noreply.github.com>
2022-05-01 15:41:51 +0300

    remove not european people

--- split_train_test_IDs_for_autoencoder.py
+++ split_train_test_IDs_for_autoencoder.py
@@ -4,16 +4,22 @@ import random
 import numpy as np
 
 file_name = '/home/hochyard/UKBB/results/chr1_imputed_snp_id_filtered_no_missing.bed' #after removing "ids_to_delete"
-#call file with Europeans' ID?????????????????????????
+ethnicity_file =  '/storage/users/nadavrap-group/UKBB/phenotypes/Ethnicity.pheno'
+ethnicity = pd.read_csv(ethnicity_file,sep=" ")
+ethnicity = ethnicity.loc[ethnicity["genetic_ethnic_grouping_f22006_0_0"] == 'Caucasian']
+ethnicity['FID']=ethnicity['FID'].astype(str)
+
 (bim, fam, bed) = read_plink(file_name, verbose=False)
 ID = fam['fid'].to_frame()
 ID = ID.rename(columns = {'fid': 'FID'}, inplace = False)
+ID = ID[ID['FID'].isin(ethnicity['FID'])].reset_index(drop=True)
+
 test_size = int(len(list(ID['FID']))*0.15)
 test_FID = random.sample(list(ID['FID']), test_size)
 df_test_FID = pd.DataFrame(test_FID,columns = ['FID']).sort_values('FID').reset_index(drop=True)
 df_train_FID = ID[~ID['FID'].isin(test_FID)].sort_values('FID').reset_index(drop=True)
 
-df_test_FID.to_csv('/home/hochyard/UKBB/results/data_for_model/test_ID.dat')
-df_train_FID.to_csv('/home/hochyard/UKBB/results/data_for_model/train_ID.dat')
+df_test_FID.to_csv('/home/hochyard/UKBB/results/data_for_model/test_ID.dat', header=False, index=False)
+df_train_FID.to_csv('/home/hochyard/UKBB/results/data_for_model/train_ID.dat', header=False, index=False)
 
 #what about the validation??????????????????
