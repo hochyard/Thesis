ebdfb5d39e81e68e5f1f05249f1f8669a4ad871d
hochyard <73438069+hochyard@users.noreply.github.com>
2022-05-01 17:33:12 +0300

    add environment variables

--- Autoencoder_for_each_chr.py
+++ Autoencoder_for_each_chr.py
@@ -3,7 +3,9 @@ from tensorflow import keras
 from tensorflow.keras import layers
 #from keras.models import Sequential
 #from keras.layers import Dense
-from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras import models
+from tensorflow.keras.callbacks import EarlyStopping
 import pickle
 from sklearn.metrics import mean_squared_error
 import random
@@ -13,21 +15,38 @@ import numpy as np
 import sys
 from pympler.asizeof import asizeof
 import torch
-
+import matplotlib.pyplot as plt
+import os
 
 def build_autoencoder(input_size):
     new_dim_snps = int(input_size * 0.1)
     #encoder = keras.Sequential([layers.Dense(new_dim_snps, input_shape=[input_size])])
     #Denoising Autoencoders
-    encoder = keras.Sequential([layers.Dense(new_dim_snps, input_shape=[input_size], activation=layers.PReLU(), activity_regularizer=regularizers.l1(10e-5))])
-    decoder = keras.Sequential([layers.Dense(input_size, input_shape=[new_dim_snps], activation=layers.PReLU())])
-    autoencoder = keras.Sequential([encoder, decoder])
-    autoencoder.compile(loss="mse", optimizer=SGD(learning_rate=0.01))
-    autoencoder.summary()
-    return autoencoder, encoder
+    # model
+    encoder = models.Sequential(name='encoder')
+    encoder.add(layer=layers.Dense(units=new_dim_snps*2, activation=layers.PReLU(), input_shape=[input_size]))
+    encoder.add(layers.Dropout(0.1))
+    encoder.add(layer=layers.Dense(units=new_dim_snps, activation=layers.PReLU()))
+    
+    decoder = models.Sequential(name='decoder')
+    decoder.add(layer=layers.Dense(units=new_dim_snps*2, activation=layers.PReLU(), input_shape=[new_dim_snps]))
+    decoder.add(layers.Dropout(0.1))
+    decoder.add(layer=layers.Dense(units=input_size, activation=layers.PReLU()))
+    
+    autoencoder = models.Sequential([encoder, decoder])
 
-def fit_autoencoder(num_epochs, X_train_chunks_file, X_val, autoencoder=None, encoder=None):
-    for epoch in range(1, num_epochs + 1):
+    #encoder = keras.Sequential([layers.Dense(new_dim_snps, input_shape=[input_size], activation=layers.PReLU(), activity_regularizer=regularizers.l1(10e-5))])
+    #decoder = keras.Sequential([layers.Dense(input_size, input_shape=[new_dim_snps], activation=layers.PReLU())])
+    #autoencoder = keras.Sequential([encoder, decoder])
+    #autoencoder.compile(loss="mse", optimizer=SGD(learning_rate=0.01,momentum=0.1))
+    autoencoder.compile(loss="mse", optimizer=Adam(learning_rate=0.00001))
+    autoencoder.summary()
+    return autoencoder
+
+def fit_autoencoder(num_epochs_from,num_epochs_to, X_train_chunks_file, X_val, autoencoder=None):
+    loss = []
+    val_loss = []
+    for epoch in range(num_epochs_from, num_epochs_to + 1):
         print('Epoch = ', epoch)
         with open(X_train_chunks_file, 'rb') as file_handle:
             batch_num = 1
@@ -36,34 +55,51 @@ def fit_autoencoder(num_epochs, X_train_chunks_file, X_val, autoencoder=None, en
                     batch = pickle.load(file_handle)
                     if epoch == 1 and batch_num == 1 and autoencoder==None:
                         number_snps = batch.shape[1]-1 #minus FID column
-                        autoencoder , encoder = build_autoencoder(number_snps)
+                        autoencoder = build_autoencoder(number_snps)
                     if batch_num == 1: #its the validation set
                         batch_num = batch_num + 1
                         continue
                     batch = batch.drop(['FID'], axis=1)
                     # fit
-                    autoencoder.fit(batch, batch, epochs=1, batch_size=200, shuffle=True, validation_data=(X_val, X_val))
+                    es = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=20, restore_best_weights=True)
+                    history = autoencoder.fit(batch, batch, epochs=1, batch_size=250, shuffle=True, validation_data=(X_val, X_val), callbacks=[es])
                     batch_num = batch_num + 1
                 except EOFError:
                     break
+        loss.extend(history.history['loss'])
+        val_loss.extend(history.history['val_loss'])
         if epoch%100 == 0:
-            save_to = "/home/hochyard/autoencoder_models_relu_act_no_cov/autoencoder_model_chr" + str(chr) + "_no_missing_" + str(epoch) + "_epochs_activation_relu_no_cov"
-            autoencoder.save(save_to)
-            save_to = "/home/hochyard/autoencoder_models_relu_act_no_cov/encoder_chr" + str(chr) + "_no_missing_" + str(epoch) + "_epochs_activation_relu_no_cov"
-            encoder.save(save_to)
-    return autoencoder, encoder
+            save_to = "/home/hochyard/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001/autoencoder_chr" + str(chr) + "_" + str(epoch) + "_epochs"
+            autoencoder.save(save_to,num_epochs_to)
+            #save_to = "/home/hochyard/autoencoder_models_5_layers_prelu_act_no_cov/encoder_chr" + str(chr) + "_" + str(epoch) + "_epochs"
+            #encoder.save(save_to)
+    plot_loss(loss, val_loss)
+    return autoencoder
+    
+def plot_loss(loss, val_loss):
+    fig = plt.figure()
+    plt.plot(loss)
+    plt.plot(val_loss)
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.xlim(0, 305)
+    plt.ylim(0, 0.15)
+    name = '/home/hochyard/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001/' + str(chr) + 'loss_autoencoder.png'
+    fig.savefig(name)
 
-def predict_new_snps(encoder, X_chunks_file, output_file):
-    with open(X_chunks_file, 'rb') as file_handle:
-        with open(output_file, 'wb') as save_file_handle:
-            while True:
-                try:
-                    batch = pickle.load(file_handle)
-                    batch = batch.drop(['FID'], axis=1)
-                    codings = encoder.predict(batch)
-                    pickle.dump(codings, save_file_handle)
-                except EOFError:
-                    break
+#def predict_new_snps(encoder, X_chunks_file, output_file):
+#    with open(X_chunks_file, 'rb') as file_handle:
+#        with open(output_file, 'wb') as save_file_handle:
+#            while True:
+#                try:
+#                    batch = pickle.load(file_handle)
+#                    batch = batch.drop(['FID'], axis=1)
+#                    codings = encoder.predict(batch)
+#                    pickle.dump(codings, save_file_handle)
+#                except EOFError:
+#                    break
 
 def create_validation_set(X_train_chunks_file):
     with open(X_train_chunks_file, 'rb') as file_handle:
@@ -74,38 +110,30 @@ def create_validation_set(X_train_chunks_file):
 #------------------------------train---------------------------
 import time
 start_time = time.time()
+print('available GPUs: ')
+print(torch.cuda.device_count())
 
 # files
-for chr in range(19,20):
+#set variable 'from', 'to'
+for chr in range(os.environ['from'],os.environ['to']):
     print(chr)
-    #load autoencoder
-    #auto_name = "autoencoder_model_chr" + str(chr) + "_no_missing_300_epochs"
+    #load autoencoder 
+    #auto_name = "/home/hochyard/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001/autoencoder_chr" + str(chr) + "_200_epochs"
     #autoencoder = keras.models.load_model(auto_name)
     #encoder_name = "encoder_chr" + str(chr) + "_no_missing_300_epochs"
     #encoder = keras.models.load_model(encoder_name)                                                    
-    X_train_chunks_file = "/home/hochyard/UKBB/results/data_for_model/chunks_for_each_chr/chr_" + str(chr) +"_height_X_train_1k_chunks_no_missing.pkl"
-    X_test_chunks_file = "/home/hochyard/UKBB/results/data_for_model/chunks_for_each_chr/chr_" + str(chr) + "_height_X_test_1k_chunks_no_missing.pkl"
+    X_train_chunks_file = "/home/hochyard/UKBB/results/data_for_model/chunks_for_each_chr/chr_" + str(chr) +"_X_train_1k_chunks_no_missing.pkl"
+    X_test_chunks_file = "/home/hochyard/UKBB/results/data_for_model/chunks_for_each_chr/chr_" + str(chr) + "_X_test_1k_chunks_no_missing.pkl"
     val_size = 1000
     X_val = create_validation_set(X_train_chunks_file)
-    autoencoder, encoder = fit_autoencoder(num_epochs=700,
-                                            X_train_chunks_file=X_train_chunks_file,
-                                            X_val=X_val,
-                                            autoencoder = None,
-                                            encoder = None)
-    #save_to = "autoencoder_model_chr" + str(chr) + "_no_missing_400_epochs"
-    #autoencoder.save(save_to)
-    #save_to = "encoder_chr" + str(chr) + "_no_missing_400_epochs"
-    #encoder.save(save_to)
+    autoencoder = fit_autoencoder(num_epochs_from=1,
+                                  num_epochs_to=8,
+                                  X_train_chunks_file=X_train_chunks_file,
+                                  X_val=X_val,
+                                  autoencoder = None)                                      
     print("--- %s seconds for data fiting---" % (time.time() - start_time))
 
-    ## predict new features for each batch
-    #train_output_file = "/home/hochyard/UKBB/results/data_for_model/chunks_for_each_chr/chr_" + str(chr) + "_height_X_train_1k_chunks_dim_remove_no_missing_400_epochs.pkl"
-    #predict_new_snps(encoder=encoder,
-    #                  X_chunks_file=X_train_chunks_file,
-    #                  output_file=train_output_file)
-# ------------------------------test-------------------------
-    #test_output_file = "/home/hochyard/UKBB/results/data_for_model/chunks_for_each_chr/chr_" + str(chr) + "_height_X_test_1k_chunks_dim_remove_no_missing_400_epochs.pkl"
-    #predict_new_snps(encoder=encoder, X_chunks_file=X_test_chunks_file, output_file=test_output_file)
+
 
 
 
--- split_each_chr_to_chunks.py
+++ split_each_chr_to_chunks.py
@@ -4,10 +4,10 @@ from pandas_plink import read_plink, read_plink1_bin
 import numpy as np
 import pickle
 from sklearn.impute import KNNImputer, SimpleImputer
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
+import os
 
-def split_X_to_chunks(cov_file, gene_end_file_name, gene_output_end_file_name, chunk_size, chr, gene_scaler):
-    #cov_df = pd.read_pickle(cov_file)
+def split_X_to_chunks(gene_end_file_name, gene_output_end_file_name, chunk_size, chr, gene_scaler):
     from_ = 0
     to_ = chunk_size
     gene_output_file = '/home/hochyard/UKBB/results/data_for_model/chunks_for_each_chr/chr_' + str(chr) + gene_output_end_file_name
@@ -23,17 +23,17 @@ def split_X_to_chunks(cov_file, gene_end_file_name, gene_output_end_file_name, c
                 G_to_pandas = G_to_pandas.astype(np.int8)
                 G_to_pandas.reset_index(level=0, inplace=True)
                 G_to_pandas.rename(columns={'sample': 'FID'}, inplace=True)
-                #df = pd.merge(cov_df, G_to_pandas, how='inner', on='FID')
-                not_to_scale = G_to_pandas['FID']
-                scaled_values = gene_scaler.transform(G_to_pandas.drop('FID', axis = 1))
-                G_to_pandas = pd.DataFrame(scaled_values, index = G_to_pandas.index, columns = G_to_pandas.drop('FID', axis = 1).columns)
-                G_to_pandas['FID'] = not_to_scale
+                if gene_scaler!=None:
+                    not_to_scale = G_to_pandas['FID']
+                    scaled_values = gene_scaler.transform(G_to_pandas.drop('FID', axis = 1))
+                    G_to_pandas = pd.DataFrame(scaled_values, index = G_to_pandas.index, columns = G_to_pandas.drop('FID', axis = 1).columns)
+                    G_to_pandas['FID'] = not_to_scale
                 pickle.dump(G_to_pandas, gene_file_handle, protocol=4)
-                #pickle.dump(df, gene_file_handle, protocol=4)
                 from_ = to_
                 to_ = to_ + chunk_size
             else:
                 return
+
 
 def split_y_to_chunks(output_file, input_file, x_chunk_file, chunk_size):
      y = pd.read_pickle(input_file)
@@ -56,7 +56,8 @@ def StandardScaler_incremental(end_file_name_to_standard, chr, chunk_size):
     to_ = chunk_size
     file_name = '/home/hochyard/UKBB/results/data_for_model/chr' + str(chr) + end_file_name_to_standard
     G = read_plink1_bin(file_name, verbose=False)  # xarray.core.dataarray.DataArray
-    chr_scaler = StandardScaler()
+    #chr_scaler = StandardScaler()
+    chr_scaler = MinMaxScaler()
     while True:  # while there are still samples
         G_to_pandas = G[from_:to_, :].to_pandas()  # pandas.core.frame.DataFrame
         if G_to_pandas.empty == False:  # their samples
@@ -64,36 +65,49 @@ def StandardScaler_incremental(end_file_name_to_standard, chr, chunk_size):
             from_ = to_
             to_ = to_ + chunk_size
         else:
-            print(chr_scaler.mean_)
-            print(chr_scaler.var_)
+            #print(chr_scaler.mean_)
+            #print(chr_scaler.var_)
             return chr_scaler
                 
 
-# --------------------train-------------------------------
-#check()
-# handle X + y
-
-for chr in range(19,20):
-    scaler = StandardScaler_incremental("_height_X_train_no_cov_no_missing.bed", chr, chunk_size=1000)
-    #train
-    split_X_to_chunks(cov_file="/home/hochyard/UKBB/results/data_for_model/height_cov_matrix_train.pkl",
-                     gene_end_file_name="_height_X_train_no_cov_no_missing.bed",
-                     gene_output_end_file_name="_height_X_train_1k_chunks_no_missing_scaled.pkl",
-                     chunk_size=1000,
-                     chr = chr,
-                     gene_scaler = scaler)
-    #test
-    split_X_to_chunks(cov_file="/home/hochyard/UKBB/results/data_for_model/height_cov_matrix_test.pkl",
-                     gene_end_file_name="_height_X_test_no_cov_no_missing.bed",
-                     gene_output_end_file_name="_height_X_test_1k_chunks_no_missing_scaled.pkl",
-                     chunk_size=1000,
-                     chr = chr,
-                     gene_scaler = scaler)
-
-#split_y_to_chunks(output_file = "/home/hochyard/UKBB/results/data_for_model/chunks_for_each_chr/height_y_train_1k_chunks_no_missing.pkl",
-#                  input_file = "/home/hochyard/UKBB/results/data_for_model/height_y_train.pkl",
-#                  x_chunk_file = "chr_1_height_X_train_1k_chunks_with_cov_no_missing.pkl",
-#                  chunk_size = 1000)
+#set variable 'from', 'to', 'model', 'y'
+#---------------for Autoencoder---------------------------
+if os.environ['model'] == autoencoder:
+    for chr in range(os.environ['from'],os.environ['to']):
+        #scaler = StandardScaler_incremental("_height_X_train_no_cov_no_missing.bed", chr, chunk_size=1000)
+        #train
+        split_X_to_chunks(gene_end_file_name="_X_train_no_cov_no_missing.bed",
+                         gene_output_end_file_name="_X_train_1k_chunks_no_missing.pkl",
+                         chunk_size=1000,
+                         chr = chr,
+                         gene_scaler = None)
+        #test
+        split_X_to_chunks(gene_end_file_name="_X_test_no_cov_no_missing.bed",
+                         gene_output_end_file_name="_X_test_1k_chunks_no_missing.pkl",
+                         chunk_size=1000,
+                         chr = chr,
+                         gene_scaler = None)
+else: 
+    #---------------for NN---------------------------
+    for chr in range(5,6):
+        #scaler = StandardScaler_incremental("_height_X_train_no_cov_no_missing.bed", chr, chunk_size=1000)
+        #train
+        split_X_to_chunks(gene_end_file_name="_height_X_train_no_cov_no_missing.bed",
+                         gene_output_end_file_name="_height_X_train_1k_chunks_no_missing.pkl",
+                         chunk_size=1000,
+                         chr = chr,
+                         gene_scaler = None)
+        #test
+        split_X_to_chunks(gene_end_file_name="_height_X_test_no_cov_no_missing.bed",
+                         gene_output_end_file_name="_height_X_test_1k_chunks_no_missing.pkl",
+                         chunk_size=1000,
+                         chr = chr,
+                         gene_scaler = None)
+if y==1:
+    split_y_to_chunks(output_file = "/home/hochyard/UKBB/results/data_for_model/chunks_for_each_chr/height_y_train_1k_chunks_no_missing.pkl",
+                      input_file = "/home/hochyard/UKBB/results/data_for_model/height_y_train.pkl",
+                      x_chunk_file = "chr_1_height_X_train_1k_chunks_with_cov_no_missing.pkl",
+                      chunk_size = 1000)
 
     
 
