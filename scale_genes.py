391e638fbfb6f6bb52d7b3cc76f80d489c38d529
Yarden Hochenberg <73438069+hochyard@users.noreply.github.com>
2022-10-19 11:33:12 +0300

    Update scale_genes.py

--- scale_genes.py
+++ scale_genes.py
@@ -8,7 +8,6 @@ def StandardScaler_incremental(train_file_name_to_standard_scale, test_file_name
     for chr in range(1, 23):
         dim_remove_file = train_file_name_to_standard_scale + str(chr) + ".pkl"
         with open(dim_remove_file, 'rb') as file_handle:
-            #chr_scaler = StandardScaler()
             chr_scaler = MinMaxScaler()
             while True:
                 try:
@@ -25,7 +24,6 @@ def scale_all_genes(file_name_to_standard_scale, scaler):
     for chr in range(1, 23):
         file_name = file_name_to_standard_scale + str(chr) + ".pkl"
         with open(file_name, 'rb') as file_handle:
-            #output_file_name = file_name_to_standard_scale + str(chr) + "_scaled.pkl"
             output_file_name = file_name_to_standard_scale + str(chr) + "_MinMax_scaled.pkl"
             with open(output_file_name, 'wb') as gene_output_file_handle:
                 while True:
@@ -41,11 +39,11 @@ def scale_all_genes(file_name_to_standard_scale, scaler):
                         break
                     
 #scale and save genes
-train_X_file_name = "/home/hochyard/my_model/PCA/X_train_1k_chunks_PCA_dim_remove_no_missing/chr_" #for PCA
-#train_X_file_name = "/home/hochyard/my_model/autoencoder/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001/X_train_1k_chunks_dim_remove_no_missing_1000_epochs/chr_" #for Autoencoder
+autoencoder_train_X_file_name = "/home/hochyard/my_model/autoencoder/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001/X_train_1k_chunks_dim_remove_no_missing_1000_epochs/chr_" #for Autoencoder
+autoencoder_test_X_file_name = "/home/hochyard/my_model/autoencoder/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001/X_test_1k_chunks_dim_remove_no_missing_1000_epochs/chr_" #for Autoencoder
+StandardScaler_incremental(autoencoder_train_X_file_name, autoencoder_test_X_file_name)
 
-#test
-test_X_file_name = "/home/hochyard/my_model/PCA/X_test_1k_chunks_PCA_dim_remove_no_missing/chr_" #for PCA
-#test_X_file_name = "/home/hochyard/my_model/autoencoder/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001/X_test_1k_chunks_dim_remove_no_missing_1000_epochs/chr_" #for Autoencoder
-StandardScaler_incremental(train_X_file_name, test_X_file_name)
+PCA_train_X_file_name = "/home/hochyard/my_model/PCA/X_train_1k_chunks_PCA_dim_remove_no_missing/chr_" #for PCA
+PCA_test_X_file_name = "/home/hochyard/my_model/PCA/X_test_1k_chunks_PCA_dim_remove_no_missing/chr_" #for PCA
+StandardScaler_incremental(PCA_train_X_file_name, PCA_test_X_file_name)
 
