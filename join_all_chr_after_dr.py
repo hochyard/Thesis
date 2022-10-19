4c6e161247e85f8869d16f21dd00a7a156de76c7
Yarden Hochenberg <73438069+hochyard@users.noreply.github.com>
2022-10-19 11:38:59 +0300

    Update join_all_chr_after_dr.py

--- join_all_chr_after_dr.py
+++ join_all_chr_after_dr.py
@@ -3,14 +3,12 @@ import pickle
 from sklearn.preprocessing import StandardScaler
 
 def join(X_file_name, chunk_size, gene_output_file, cov_df, gene_scale):
-    number_samples = 0 
     with open(gene_output_file, 'wb') as gene_output_file_handle:
         this_chunk = 1
         while True:
             try:
                 for chr in range(1, 23):
                     dim_remove_file = X_file_name + str(chr) 
-                    #dim_remove_file = dim_remove_file + "_scaled.pkl" if gene_scale else dim_remove_file + ".pkl"
                     dim_remove_file = dim_remove_file + "_MinMax_scaled.pkl" if gene_scale else dim_remove_file + ".pkl"
                     with open(dim_remove_file, 'rb') as dim_remove_file_handle:
                         for c in range(1,this_chunk+1):
@@ -29,30 +27,27 @@ def join(X_file_name, chunk_size, gene_output_file, cov_df, gene_scale):
             except EOFError:
                 break
 
-
-                
- 
 # handle covariate
-cov_df = pd.read_pickle('/home/hochyard/UKBB/results/data_for_model/cov_matrix_MinMax_scaled_no_missing.pkl')
-remove =[]
-for i in range(1,41):
-    genetic_str = 'genetic_principal_components_f22009_0_' + str(i)
-    remove += [genetic_str]
-cov_df = cov_df.drop(remove, axis=1)
+cov_df = pd.read_pickle('/home/hochyard/UKBB/results/data_for_model/cov_matrix_MinMax_scaled_no_missing_no_40PCA.pkl')
 chunk_size=1000
 gene_scale = True
-train_X_file_name = "/home/hochyard/my_model/PCA/X_train_1k_chunks_PCA_dim_remove_no_missing/chr_" #for PCA
-#train_X_file_name = "/home/hochyard/my_model/autoencoder/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001/X_train_1k_chunks_dim_remove_no_missing_1000_epochs/chr_" #for autoencoder
 
-test_X_file_name = "/home/hochyard/my_model/PCA/X_test_1k_chunks_PCA_dim_remove_no_missing/chr_" #for PCA
-#test_X_file_name = "/home/hochyard/my_model/autoencoder/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001/X_test_1k_chunks_dim_remove_no_missing_1000_epochs/chr_" #for Autoencoder
-
+#------------autoencoder---------------
+autoencoder_train_X_file_name = "/home/hochyard/my_model/autoencoder/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001_batch_size250/X_train_1k_chunks_dim_remove_no_missing_600_epochs/chr_" 
+autoencoder_test_X_file_name = "/home/hochyard/my_model/autoencoder/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001_batch_size250/X_test_1k_chunks_dim_remove_no_missing_600_epochs/chr_" 
 #train
-union_train_gene_output_file = "/home/hochyard/my_model/PCA/X_train_1k_chunks_PCA_dim_remove_no_missing/all_chr_MinMax_scaled_cov_MinMax_scaled_no_40PCA.pkl" #for PCA
-#union_train_gene_output_file = "/home/hochyard/my_model/autoencoder/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001/X_train_1k_chunks_dim_remove_no_missing_1000_epochs/all_chr_MinMax_scaled_cov_MinMax_scaled.pkl" #for Autoencoder
-join(train_X_file_name, chunk_size, union_train_gene_output_file, cov_df, gene_scale)
-
+autoencoder_union_train_gene_output_file = "/home/hochyard/my_model/autoencoder/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001_batch_size250/X_train_1k_chunks_dim_remove_no_missing_600_epochs/all_chr_MinMax_scaled_cov_MinMax_scaled_no_40PCA.pkl"
+join(autoencoder_train_X_file_name, chunk_size, autoencoder_union_train_gene_output_file, cov_df, gene_scale)
 #test
-union_test_gene_output_file = "/home/hochyard/my_model/PCA/X_test_1k_chunks_PCA_dim_remove_no_missing/all_chr_MinMax_scaled_cov_MinMax_scaled_no_40PCA.pkl" #for PCA
-#union_test_gene_output_file = "/home/hochyard/my_model/autoencoder/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001/X_test_1k_chunks_dim_remove_no_missing_1000_epochs/all_chr_MinMax_scaled_cov_MinMax_scaled.pkl" #for Autoencoder
-join(test_X_file_name, chunk_size, union_test_gene_output_file, cov_df, gene_scale)
+autoencoder_union_test_gene_output_file = "/home/hochyard/my_model/autoencoder/autoencoder_models_5_layers_prelu_act_no_cov_adam0.00001_batch_size250/X_test_1k_chunks_dim_remove_no_missing_600_epochs/all_chr_MinMax_scaled_cov_MinMax_scaled_no_40PCA.pkl" 
+join(autoencoder_test_X_file_name, chunk_size, autoencoder_union_test_gene_output_file, cov_df, gene_scale)
+
+#------------PCA---------------
+PCA_train_X_file_name = "/home/hochyard/my_model/PCA/X_train_1k_chunks_PCA_dim_remove_no_missing/chr_" 
+PCA_test_X_file_name = "/home/hochyard/my_model/PCA/X_test_1k_chunks_PCA_dim_remove_no_missing/chr_" 
+#train
+PCA_union_train_gene_output_file = "/home/hochyard/my_model/PCA/X_train_1k_chunks_PCA_dim_remove_no_missing/all_chr_MinMax_scaled_cov_MinMax_scaled_no_40PCA.pkl" 
+join(PCA_train_X_file_name, chunk_size, PCA_union_train_gene_output_file, cov_df, gene_scale)
+#test
+PCA_union_test_gene_output_file = "/home/hochyard/my_model/PCA/X_test_1k_chunks_PCA_dim_remove_no_missing/all_chr_MinMax_scaled_cov_MinMax_scaled_no_40PCA.pkl" 
+join(PCA_test_X_file_name, chunk_size, PCA_union_test_gene_output_file, cov_df, gene_scale)
